2133f762b2329d5836fb4072ef43e3c4fadd6101
Peregriney <45464810+Peregriney@users.noreply.github.com>
2024-09-01 01:46:43 -0700

    Update pkgimport.jl

--- pkgimport.jl
+++ pkgimport.jl
@@ -1,1 +1,1 @@
-import Pkg; Pkg.add("CSV"); Pkg.add("DataFrames"); Pkg.add("Plots");Pkg.add("Permutations");Pkg.add("Tables");Pkg.add("StatsBase");Pkg.add("TOML");
+import Pkg; Pkg.add("CSV"); Pkg.add("DataFrames"); Pkg.add("Plots");Pkg.add("Permutations");Pkg.add("Tables");Pkg.add("StatsBase");Pkg.add("TOML");Pkg.add("Combinations")
